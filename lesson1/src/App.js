385fae0d6197788096276d38d5696cc39b369d93
PhyoPyae94 <kirudozack@gmail.com>
2019-04-04 01:24:51 +0630

    setup L-2

---
+++ lesson1/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Intro from './Intro.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Logo/>
+          <Intro/>
+      </div>
+    );
+  }
+}
+
+class Logo extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+            <LogoText/>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
+}
+class LogoText extends Logo {
+  render() {
+    return (
+      <div>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ lesson1/src/Intro.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Intro extends Component {
+    render() {
+      return (
+        <div>
+            <h2>Title</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia illo accusantium nobis ex doloribus consectetur dolorum distinctio consequuntur in dicta vel perspiciatis, minima, delectus adipisci fugiat corporis voluptatem eius culpa!</p>
+        </div>
+      );
+    }
+  }
+
+export default Intro;
