9c333427bf4fdf19a85d7b8ecd6f912750070b53
PhyoPyae94 <kirudozack@gmail.com>
2019-04-04 17:46:34 +0630

    properties(props) in react

--- lesson2/src/App.js
+++ lesson2/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -20,12 +19,34 @@ class App extends Component {
     const title = 'Calling Function';
     return (
       <div className="App">
+        <h2>{title}</h2>
         <h2>First Title: {this.calling('react')} {1+1}</h2>
         {/* you cant call the other method without (this) keyword */}
 
         <h2>Second Title: {this.yourname}</h2>
+        <hr/>
+        <Marvel msg="marvel's heros are cool" favHeros="Deadpool" coolHeros={this.props.heros}/>
       </div>
     );
+  }
+}
+
+App.defaultProps = {
+  heros: ['antman', 'ironman', 'dr.strange', 'spiderman']
+}
+class Marvel extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+          <h2>I'm a marvel fun.</h2>
+          <p>{this.props.msg}</p>
+          <p>{this.props.favHeros}</p>
+          <p>{this.props.coolHeros.map((item, i) => {
+            return " " + item;
+          })}</p>
+      </div>
+    )
   }
 }
 
